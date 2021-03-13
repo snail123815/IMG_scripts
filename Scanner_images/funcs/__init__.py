31180c4e30f92c63e496b0b22024199c4985b114
snail123815 <durand.dc@gmail.com>
2021-03-13 19:31:06 +0100

    give birth to scanLapsePlot

--- Scanner_images/funcs/__init__.py
+++
@@ -1,6 +0,0 @@
-from .parseMetadata import getInfo, getPositions, getPosToCrop
-from .changeName import changeFileName, getScanTime, determinePrefixExtension, determineExtension
-from .misc import createFolders
-from .measureImages import measureImgs
-from .plotting import plotMeasured
-from .crop import crop
